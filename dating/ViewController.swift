8d60d7e0c4ea75a2245586329e87e3470c9c73d6
adamsun <xsun@ualberta.ca>
2021-02-22 16:51:24 -0700

    Initial Commit

---
+++ dating/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dating
+//
+//  Created by Xiao Sun on 2021-02-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
